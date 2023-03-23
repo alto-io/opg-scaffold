925214de3507481b7f8bbc63555d9871190de562
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-03-23 11:07:34 +0000

    Add license: UNLICENSED to all contracts

--- packages/solidity-ts/contracts/arcadiaDiamonds/inventory/InventoryInternal.sol
+++ packages/solidity-ts/contracts/arcadiaDiamonds/inventory/InventoryInternal.sol
@@ -1,13 +1,11 @@
-
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
 import { RolesInternal } from "../roles/RolesInternal.sol";
 import { ReentrancyGuard } from "@solidstate/contracts/utils/ReentrancyGuard.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import { ERC1155BaseInternal } from "@solidstate/contracts/token/ERC1155/base/ERC1155BaseInternal.sol";
 import { InventoryStorage } from "./InventoryStorage.sol";
-import { ItemsStorage } from "../items/ItemsStorage.sol";
 import { AddressUtils } from "@solidstate/contracts/utils/AddressUtils.sol";
 
 contract InventoryInternal is
