40e633ba2ab281f75148960f3f7f278c6401fde6
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-04-01 01:14:23 +0100

    Move support interface call to initialiser so diamond is can remain immutable in the future

--- packages/solidity-ts/contracts/arcadiaDiamonds/arcadians/ArcadiansDiamond.sol
+++ packages/solidity-ts/contracts/arcadiaDiamonds/arcadians/ArcadiansDiamond.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.19;
 
 import { SolidStateDiamond } from '@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol';
-import { IERC721 } from '@solidstate/contracts/interfaces/IERC721.sol';
 
 /**
  * @title ArcadiansDiamond
@@ -11,8 +10,4 @@ import { IERC721 } from '@solidstate/contracts/interfaces/IERC721.sol';
  * which allows it to act as a proxy for a collection of other contracts.
  * This contract specifically supports the ERC-721 standard, which is used to store and manage the Arcadians NFTs.
  */
-contract ArcadiansDiamond is SolidStateDiamond {
-    constructor() {
-        _setSupportsInterface(type(IERC721).interfaceId, true);
-    }
-}
+contract ArcadiansDiamond is SolidStateDiamond {}
