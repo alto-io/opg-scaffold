b59c576406993b378af2ebe3ddb47144f1bd869b
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-04-01 01:14:23 +0100

    Move support interface call to initialiser so diamond is can remain immutable in the future

--- packages/solidity-ts/contracts/arcadiaDiamonds/items/ItemsDiamond.sol
+++ packages/solidity-ts/contracts/arcadiaDiamonds/items/ItemsDiamond.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.19;
 
 import { SolidStateDiamond } from '@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol';
-import { IERC1155 } from '@solidstate/contracts/interfaces/IERC1155.sol';
 
 /**
  * @title ItemsDiamond
@@ -11,8 +10,4 @@ import { IERC1155 } from '@solidstate/contracts/interfaces/IERC1155.sol';
  * which allows it to act as a proxy for a collection of other contracts.
  * This contract specifically supports the ERC-1155 standard, which is used to store and manage the Arcadians Items.
  */
-contract ItemsDiamond is SolidStateDiamond {
-    constructor() {
-        _setSupportsInterface(type(IERC1155).interfaceId, true);
-    }
-}
+contract ItemsDiamond is SolidStateDiamond {}
--- packages/solidity-ts/contracts/arcadiaDiamonds/items/ItemsInit.sol
+++ packages/solidity-ts/contracts/arcadiaDiamonds/items/ItemsInit.sol
@@ -6,9 +6,14 @@ import { MerkleStorage } from "../merkle/MerkleStorage.sol";
 import { RolesInternal } from "../roles/RolesInternal.sol";
 import { ItemsInternal } from "./ItemsInternal.sol";
 import { InventoryInternal } from "../inventory/InventoryInternal.sol";
+import { ERC165BaseInternal } from '@solidstate/contracts/introspection/ERC165/base/ERC165BaseInternal.sol';
+import { IERC1155 } from '@solidstate/contracts/interfaces/IERC1155.sol';
 
-contract ItemsInit is RolesInternal, ItemsInternal, InventoryInternal {    
+contract ItemsInit is RolesInternal, ItemsInternal, InventoryInternal, ERC165BaseInternal {    
     function init(bytes32 merkleRoot, string calldata baseUri) external {
+
+        _setSupportsInterface(type(IERC1155).interfaceId, true);
+
         MerkleStorage.Layout storage es = MerkleStorage.layout();
         es.merkleRoot = merkleRoot;
 
