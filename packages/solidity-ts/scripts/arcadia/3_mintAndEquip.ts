27abdedf29af342e2e79c20af6abb8a160aabffe
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-04-24 17:44:54 +0100

    Reword scripts to interact with the SC

--- packages/solidity-ts/scripts/arcadia/3_mintAndEquip.ts
+++ packages/solidity-ts/scripts/arcadia/3_mintAndEquip.ts
@@ -1,8 +1,6 @@
 // import { ethers } from "ethers";
 import hre from "hardhat";
-import fs from "fs";
-import { BigNumber, ethers } from "ethers";
-import path from "path";
+import { BigNumber } from "ethers";
 import getDeployedContracts from "./utils/deployedContracts";
 
 enum SlotCategory { Base, Equippment, Cosmetic}
@@ -22,35 +20,52 @@ async function main() {
     const network = hre.network.name;
     const { itemsSC, inventorySC, arcadiansSC } = await getDeployedContracts(network);
 
+
     const arcadianId = 1;
     const slotsAll: Slot[] = await inventorySC.slotsAll();
     
     const itemsToEquip: Item[] = [];
-    const slotsIds: number[] = [];
     for (const slot of slotsAll) {
         const numAllowedItems: BigNumber = await inventorySC.numAllowedItems(slot.id)
         
         if (numAllowedItems.gt(0)) {
             const itemToEquip: Item = await inventorySC.allowedItem(slot.id, numAllowedItems.sub(1))
             itemsToEquip.push(itemToEquip);
-
-            slotsIds.push((slot.id as any).toNumber())
         }
     }
     await itemsSC.setApprovalForAll(inventorySC.address, true);
-    
-    console.log("signer address: ", await inventorySC.signer.getAddress());
-    const itemsIds = itemsToEquip.map((item)=> item.id.toString())
-    console.log("itemsIds: ", itemsIds)
-    let tx = await inventorySC.equipBatch(arcadianId, slotsIds, itemsToEquip);
-    await tx.wait();
+
+    const isPublicMintOpen = await await arcadiansSC.publicMintOpen();
+    if (!isPublicMintOpen) {
+        let tx = await arcadiansSC.openPublicMint();
+        tx.wait()
+    }
 
     const equippedAll = await inventorySC.equippedAll(arcadianId);
     console.log("Items equipped in all slots: ", equippedAll);
+    
+
+    const payAmount = await arcadiansSC.mintPrice()
+    const mintAmount = 1;
+    for (let i = 0; i < mintAmount; i++) {
+        let tx = await arcadiansSC.mintAndEquip(itemsToEquip, {value: payAmount});
+        await tx.wait();
+    }
+    const minterAddress = await arcadiansSC.signer.getAddress();
+    
+    console.log("minterAddress: ", minterAddress);
+    const balance = await arcadiansSC.balanceOf(minterAddress);
+    console.log("balance arcadians: ", balance);
+    const ownedTokens = [];
+    for (let i = 0; i < balance; i++) {
+        const arcadianId: BigNumber = await arcadiansSC.tokenOfOwnerByIndex(minterAddress, i);
+        ownedTokens.push(arcadianId.toNumber());
+    }
+    console.log("owned arcadians: ", ownedTokens);
 }
 
 main().catch((error) => {
     console.error("error: ", error);
     process.exitCode = 1;
-  });
+});
   
---
+++ packages/solidity-ts/scripts/arcadia/4_unequipItems.ts
@@ -0,0 +1,52 @@
+// import { ethers } from "ethers";
+import hre, { ethers } from "hardhat";
+import getDeployedContracts from "./utils/deployedContracts";
+import { ItemInSlot } from "./5_fetchDataSC";
+
+enum SlotCategory { Base, Equippment, Cosmetic}
+interface Slot {
+    id?: number,
+    permanent: boolean,
+    category: SlotCategory,
+    allowedItems?: number[]
+}
+
+interface Item {
+    erc721Contract: string,
+    id: number
+}
+
+async function main() {
+
+    const network = hre.network.name;
+    const { itemsSC, inventorySC, arcadiansSC } = await getDeployedContracts(network);
+
+    const arcadianId = 1;
+    let equippedAll: ItemInSlot[] = await inventorySC.equippedAll(arcadianId);
+    let slotsAll: Slot[] = await inventorySC.slotsAll();
+    
+    const slotsIds = equippedAll.filter((item: ItemInSlot)=>item.erc721Contract != ethers.constants.AddressZero).map((item: ItemInSlot)=>item.slotId);
+    
+    const slotsIdsToUnequip: number[] = [];
+    for (let i = 0; i < slotsIds.length; i++) {
+        const slotId = slotsIds[i];
+        const slot = slotsAll.find((s)=> Number(s.id) == Number(slotId));
+        
+        if (slot && !slot.permanent && slot.category != SlotCategory.Base) {
+            slotsIdsToUnequip.push(Number(slot.id));
+        }
+    }
+    console.log("slots Ids to unequip: ", slotsIdsToUnequip);
+    
+    let tx = await inventorySC.unequip(arcadianId, slotsIdsToUnequip);
+    await tx.wait();
+
+    equippedAll = await inventorySC.equippedAll(arcadianId);
+    console.log("Items equipped in all slots: ", equippedAll);
+}
+
+main().catch((error) => {
+    console.error("error: ", error);
+    process.exitCode = 1;
+  });
+  
