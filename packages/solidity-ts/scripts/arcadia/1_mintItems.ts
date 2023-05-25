810a479f34bbeec84324a8aa8657b5bba06196d3
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-05-25 10:43:55 +0100

    Mint only basic items

--- packages/solidity-ts/scripts/arcadia/1_mintItems.ts
+++ packages/solidity-ts/scripts/arcadia/1_mintItems.ts
@@ -2,25 +2,31 @@ import hre from "hardhat";
 import getDeployedContracts from "./utils/deployedContracts";
 import fs from "fs";
 import path from "path";
-import { Item, ItemKeys, itemsPath } from "./utils/interfaces";
+import { Item, ItemKeys, Slot, itemsPath, slotsPath } from "./utils/interfaces";
 
 const itemsAll: Item[] = JSON.parse(fs.readFileSync(itemsPath).toString());
+const slotsAll: Slot[] = JSON.parse(fs.readFileSync(slotsPath).toString());
 
 async function main() {
     const network = hre.network.name;
-    const { itemsSC, arcadiansSC } = await getDeployedContracts(network);
+    const { itemsSC } = await getDeployedContracts(network);
     const accounts = await hre.getNamedAccounts();
 
-    const recipientAddress = await arcadiansSC.signer.getAddress();
+    const recipientAddress = await itemsSC.signer.getAddress();
     console.log("Items recipient address: ", recipientAddress);
     const maxItemsPerTransaction = 100;
 
-    // Mint items
-    for (let i = 0; i <= itemsAll.length; i += maxItemsPerTransaction) {
+    const nonBasicItems = itemsAll.filter((item)=>{
+        const itemSlot = slotsAll.find((slot)=>slot.id==item.slotId)
+        return !item.isBasic && itemSlot && !itemSlot.isBase && !itemSlot.permanent
+    })
 
-        let mintItemsTx: Item[] = itemsAll.slice(i, i + maxItemsPerTransaction)
+    // Mint items
+    for (let i = 0; i <= nonBasicItems.length; i += maxItemsPerTransaction) {
+
+        let mintItemsTx: Item[] = nonBasicItems.slice(i, i + maxItemsPerTransaction)
         const itemsIds = mintItemsTx.map((item)=>item.id);
-        const itemsAmounts = mintItemsTx.map((item)=>item.mintAmount);
+        const itemsAmounts = mintItemsTx.map((item)=>1);
         
         let tx = await itemsSC.mintBatch(recipientAddress, itemsIds, itemsAmounts);
         await tx.wait();
